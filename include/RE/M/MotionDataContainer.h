19b7d75576e0b104c66ad4607101234affbd2b8d
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2023-12-16 12:12:59 +0800

    Nested half into MotionDataContainer

--- include/RE/M/MotionDataContainer.h
+++ include/RE/M/MotionDataContainer.h
@@ -1,16 +1,43 @@
 #pragma once
 
-#include <external/half.hpp>
-
 #include "RE/N/NiPoint3.h"
 #include "RE/N/NiQuaternion.h"
 
 namespace RE
 {
-	using half = half_float::half;
-
 	class MotionDataContainer
 	{
+		class half
+		{
+		public:
+			//half(std::uint16_t a_value)
+			//: value{ a_value }
+			//{ }
+
+			half(float a_value) :
+				value{ _mm_cvtps_ph([&a_value]() -> __m128 {
+					__m128 value128;
+					value128.m128_f32[0] = a_value;
+
+					return value128;
+				}(),
+					1)
+						   .m128i_u16[0] }
+			{}
+
+			inline operator float()
+			{
+				__m128i value128;
+
+				value128.m128i_u16[0] = value;
+
+				return _mm_cvtph_ps(value128).m128_f32[0];
+			}
+
+		private:
+			std::uint16_t value;
+		};
+
 	public:
 		// Translation
 		constexpr bool IsTranslationDataAligned() const
